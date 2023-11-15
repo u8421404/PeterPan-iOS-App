d471507751e5e43a7d901d003ee15b1a8db1e785
Patrick Hsieh <u8421404@users.noreply.github.com>
2023-11-15 10:27:31 +0800

    2023.11.15 1️⃣－1️⃣ ⭐️ 熟悉 App gif 錄製，GitHub，Medium

---
+++ HW1-1/HW1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1-1
+//
+//  Created by Patrick Hsieh on 2023/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
